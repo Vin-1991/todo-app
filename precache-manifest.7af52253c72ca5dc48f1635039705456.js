d4f82b29286b143cc77457f1ce56139f154122d3
Vinay Aggarwal <vinayaggarwal5204@gmail.com>
2020-09-03 12:18:27 +0530

    Updates

---
+++ precache-manifest.7af52253c72ca5dc48f1635039705456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b305836fb7a230883c68770ac08cc60",
+    "url": "/Vin-1991/todo-app/index.html"
+  },
+  {
+    "revision": "6ea7d2e8c9535d822b0c",
+    "url": "/Vin-1991/todo-app/static/css/main.5b8b4006.chunk.css"
+  },
+  {
+    "revision": "d659b675d4158f4169c6",
+    "url": "/Vin-1991/todo-app/static/js/2.5a4a57c6.chunk.js"
+  },
+  {
+    "revision": "b14f6f3561fb78329b99f9a073b7b3a4",
+    "url": "/Vin-1991/todo-app/static/js/2.5a4a57c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea7d2e8c9535d822b0c",
+    "url": "/Vin-1991/todo-app/static/js/main.22cae734.chunk.js"
+  },
+  {
+    "revision": "0c402e379db5d2bcdf20",
+    "url": "/Vin-1991/todo-app/static/js/runtime-main.535cc6dd.js"
+  }
+]);
